6349909d84060b443e85e1b2436bc0629ff69fb2
Xavier Barrachina <xbarrachinafont@gmail.com>
2024-03-11 23:38:14 +0100

    new functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 const alphabetContainer = document.getElementById("alphabet-container");
+const valorBusqueda = document
+  .getElementById("inputBusqueda")
+  .value.toLowerCase();
 
 for (let i = 65; i <= 90; i++) {
   // Códigos ASCII de A a Z
@@ -27,31 +30,125 @@ for (let i = 65; i <= 90; i++) {
 }
 
 // ------------------------------------------------------------------------
-let contacts = [];
+const contacts = [
+  {
+    nombre: "Juan",
+    apellido: "Pérez",
+    telefono: "555-1234",
+    email: "juan.perez@example.com",
+    foto: "https://picsum.photos/215",
+  },
+  {
+    nombre: "María",
+    apellido: "Gómez",
+    telefono: "555-5678",
+    email: "maria.gomez@example.com",
+    foto: "https://picsum.photos/214",
+  },
+  {
+    nombre: "Carlos",
+    apellido: "Rodríguez",
+    telefono: "555-9876",
+    email: "carlos.rodriguez@example.com",
+    foto: "https://picsum.photos/213",
+  },
+  {
+    nombre: "Ana",
+    apellido: "López",
+    telefono: "555-4321",
+    email: "ana.lopez@example.com",
+    foto: "https://picsum.photos/212",
+  },
+  {
+    nombre: "Pedro",
+    apellido: "Martínez",
+    telefono: "555-8765",
+    email: "pedro.martinez@example.com",
+    foto: "https://picsum.photos/211",
+  },
+  {
+    nombre: "Laura",
+    apellido: "García",
+    telefono: "555-1111",
+    email: "laura.garcia@example.com",
+    foto: "https://picsum.photos/210",
+  },
+  {
+    nombre: "Antonio",
+    apellido: "Sánchez",
+    telefono: "555-2222",
+    email: "antonio.sanchez@example.com",
+    foto: "https://picsum.photos/209",
+  },
+  {
+    nombre: "Isabel",
+    apellido: "Fernández",
+    telefono: "555-3333",
+    email: "isabel.fernandez@example.com",
+    foto: "https://picsum.photos/208",
+  },
+  {
+    nombre: "Miguel",
+    apellido: "Díaz",
+    telefono: "555-4444",
+    email: "miguel.diaz@example.com",
+    foto: "https://picsum.photos/207",
+  },
+  {
+    nombre: "Carmen",
+    apellido: "Ruiz",
+    telefono: "555-5555",
+    email: "carmen.ruiz@example.com",
+    foto: "https://picsum.photos/206",
+  },
+  {
+    nombre: "Javier",
+    apellido: "Gutiérrez",
+    telefono: "555-6666",
+    email: "javier.gutierrez@example.com",
+    foto: "https://picsum.photos/205",
+  },
+  {
+    nombre: "Sofía",
+    apellido: "Hernández",
+    telefono: "555-7777",
+    email: "sofia.hernandez@example.com",
+    foto: "https://picsum.photos/204",
+  },
+  {
+    nombre: "Alejandro",
+    apellido: "Jiménez",
+    telefono: "555-8888",
+    email: "alejandro.jimenez@example.com",
+    foto: "https://picsum.photos/203",
+  },
+  {
+    nombre: "Elena",
+    apellido: "Torres",
+    telefono: "555-9999",
+    email: "elena.torres@example.com",
+    foto: "https://picsum.photos/202",
+  },
+  {
+    nombre: "Francisco",
+    apellido: "Navarro",
+    telefono: "555-0000",
+    email: "francisco.navarro@example.com",
+    foto: "https://picsum.photos/201",
+  },
+];
 let timerId;
 
-// Genera 10 contactos ficticios
-for (let i = 1; i <= 10; i++) {
-  const nombre = `Contacto${i}`;
-  const apellido = `Apellido${i}`;
-  const telefono = `555-123-${i.toString().padStart(2, "0")}`;
-  const email = `contacto${i}@example.com`;
-  const foto = `https://via.placeholder.com/150?text=Foto${i}`;
-
-  const newContact = {
-    nombre: nombre,
-    apellido: apellido,
-    telefono: telefono,
-    email: email,
-    foto: foto,
-  };
-
-  // Agrega el nuevo contacto al array contactos
-  contacts.push(newContact);
-}
 console.log(contacts);
 
 //--------------------------------//
+
+const newContact = () => {
+  containerForm.classList.remove("hidden");
+};
+const closeContainer = () => {
+  containerForm.classList.add("hidden");
+};
 
 function mostrarResultados(resultados) {
   const resultadosContainer = document.getElementById("resultados");
@@ -82,10 +179,10 @@ function mostrarResultados(resultados) {
       <div class="flip-card-back">
         <div>
           <p class="title">${contact.nombre}</p>
-          <p class="title last-name">${contact.apellido}</p>
+          <p class="title">${contact.apellido}</p>
         </div>
         <p>${contact.telefono}</p>
-        <p>${contact.email}</p>
+        <p class="email">${contact.email}</p>
       </div>
     </div>
       `;
@@ -102,33 +199,50 @@ const inputBusqueda = document.getElementById("inputBusqueda");
 
 // Función para realizar la búsqueda después de un retraso
 function buscarDespuesDeRetraso() {
-  // Cancela el temporizador si ya está en ejecución
-  clearTimeout(timerId);
+  // Obtén el valor de búsqueda
+  const valorBusqueda = document
+    .getElementById("inputBusqueda")
+    .value.trim()
+    .toLowerCase();
 
-  // Inicia un nuevo temporizador para realizar la búsqueda después de 1 segundo
-  timerId = setTimeout(() => {
-    const valorBusqueda = document
-      .getElementById("inputBusqueda")
-      .value.toLowerCase();
-    const resultados = [];
+  // Si hay un valor de búsqueda, inicia el temporizador
+  if (valorBusqueda !== "") {
+    // Cancela el temporizador si ya está en ejecución
+    clearTimeout(timerId);
 
-    // Itera sobre cada contacto en el array de contactos
-    contacts.forEach((contacto) => {
-      // Verifica si el valor de búsqueda coincide con algún campo del contacto
-      if (
-        contacto.nombre.toLowerCase().includes(valorBusqueda) ||
-        contacto.apellido.toLowerCase().includes(valorBusqueda) ||
-        contacto.telefono.toLowerCase().includes(valorBusqueda) ||
-        contacto.email.toLowerCase().includes(valorBusqueda)
-      ) {
-        resultados.push(contacto);
-      }
-    });
-
-    // Muestra los resultados en la interfaz de usuario
-    mostrarResultados(resultados);
-  }, 500); // Espera 1 segundo antes de realizar la búsqueda
+    // Inicia un nuevo temporizador para realizar la búsqueda después de 1 segundo
+    timerId = setTimeout(() => {
+      realizarBusqueda(valorBusqueda);
+    }, 500); // Espera 1 segundo antes de realizar la búsqueda
+  } else {
+    // Si no hay valor de búsqueda, muestra todos los resultados
+    mostrarResultados(contacts);
+  }
 }
+
+// Función para realizar la búsqueda y mostrar los resultados
+function realizarBusqueda(valorBusqueda) {
+  const resultados = [];
+
+  // Itera sobre cada contacto en el array de contactos
+  contacts.forEach((contacto) => {
+    // Verifica si el valor de búsqueda coincide con algún campo del contacto
+    if (
+      contacto.nombre.toLowerCase().includes(valorBusqueda) ||
+      contacto.apellido.toLowerCase().includes(valorBusqueda) ||
+      contacto.telefono.toLowerCase().includes(valorBusqueda) ||
+      contacto.email.toLowerCase().includes(valorBusqueda)
+    ) {
+      resultados.push(contacto);
+    }
+  });
+
+  // Muestra los resultados en la interfaz de usuario
+  mostrarResultados(resultados);
+}
+setTimeout(() => {
+  realizarBusqueda(valorBusqueda);
+}, 500);
 
 // Escucha el evento input en el campo de búsqueda
 document
@@ -140,6 +254,8 @@ document
 const botonadd = document.getElementById("submitAdd");
 const contactForm = document.getElementById("formAdd");
 const containerForm = document.getElementById("containerForm");
+
+botonadd.addEventListener("submit", realizarBusqueda());
 
 function addContact(event) {
   event.preventDefault(); //Prevenimos las acciones predefinidas del submit. Evitamos recargar la página y enviar los datos a un servidor
